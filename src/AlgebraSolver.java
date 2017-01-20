f0f8a71c590cfc3b397463d57fcc4da622ce7b84
League <league@Mac-mini.local>
2017-01-19 18:28:46 -0800

    today we made a height calculator and started an algebra solver code.

---
+++ src/AlgebraSolver.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class AlgebraSolver {
+	public static void main(String[] args) {
+		String algebra = JOptionPane.showInputDialog(null, "enter a multiplier");
+		String answer = JOptionPane.showInputDialog(null, "enter an answer");
+	}
+
+}
